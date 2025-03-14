437a38b876718e466588788726c37c211cc150ea
ilone-a <ilonely013@gmail.com>
2025-03-15 02:00:47 +0300

    SFML invert y

--- src/ObjectLoader.cpp
+++ src/ObjectLoader.cpp
@@ -22,7 +22,7 @@ std::vector<Triangle> ObjLoader::Load(const std::string& path) {
 		if (type == "v") {
 			float x, y, z;
 			iss >> x >> y >> z;
-			vertices.push_back({ x, y, z });
+			vertices.push_back({ x, -y, z });
 		}
 		else if (type == "f") {
 			int v0, v1, v2;
